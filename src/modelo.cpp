9c816bdb06d577b424011cc919b081ebc5063d49
Matheus Abreu <matheusa596@gmail.com>
2023-02-23 07:55:27 -0300

    Fazendo o GUROBI detectar o modelo das topologias como sendo convexo.

--- src/modelo.cpp
+++ src/modelo.cpp
@@ -9,11 +9,16 @@ GRBModel criarModelo(
     std::vector<Aresta> &arestas
 ) {
     GRBModel model = GRBModel(env);
-    model.set(GRB_IntParam_NonConvex, 2);
 
     std::vector<GRBVar> d;
+    std::vector<std::vector<GRBVar>> t;
     for (size_t i = 0; i < arestas.size(); i++) {
         d.push_back(model.addVar(0, GRB_INFINITY, 1, GRB_CONTINUOUS, 'd' + std::to_string(i)));
+
+        t.push_back(std::vector<GRBVar>());
+        for (int k = 0; k < DIMENSAO; k++) {
+            t[i].push_back(model.addVar(-GRB_INFINITY, GRB_INFINITY, 0, GRB_CONTINUOUS, "t_" + std::to_string(i) + ',' + std::to_string(k + 1)));
+        }
     }
 
     GRBVar x[DIMENSAO][DIMENSAO];
@@ -35,15 +40,19 @@ GRBModel criarModelo(
             coordenadas[coordenada_ind].second 
         };
 
-        for (int i = 0; i < DIMENSAO; i++) {
-            expr += (x[x_ind][i] - coordenada[i]) * (x[x_ind][i] - coordenada[i]);
+        for (int k = 0; k < DIMENSAO; k++) {
+            model.addConstr(t[i][k] == x[x_ind][k] - coordenada[k]);
+            expr += t[i][k] * t[i][k];
         }
 
         model.addQConstr(d[i] * d[i] >= expr, 'd' + std::to_string(aresta.first) + '_' + std::to_string(aresta.second));
     }
 
-    GRBQuadExpr expr = (x[0][0] - x[1][0]) * (x[0][0] - x[1][0]);
-    expr += (x[0][1] - x[1][1]) * (x[0][1] - x[1][1]);
+    GRBQuadExpr expr = 0;
+    for (int k = 0; k < DIMENSAO; k++) {
+        model.addConstr(t[4][k] == x[0][k] - x[1][k]);
+        expr += t[4][k] * t[4][k];
+    }
 
     model.addQConstr(d[4] * d[4] >= expr, "d_{4,5}");
 
